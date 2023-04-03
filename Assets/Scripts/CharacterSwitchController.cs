aab4ab0f28123b34b8aa956a76580072605fbd2c
yuzusiooooo <70190614+yuzusiooooo@users.noreply.github.com>
2023-04-03 13:49:41 +1000

    2 characters and character switching via playerManager added

---
+++ Assets/Scripts/CharacterSwitchController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CharacterSwitchController : MonoBehaviour
+{
+    public GameObject[] CharacterPrefabs;
+    GameObject currentCharacter;
+    Vector3 charPosition;
+    public CameraFollow MainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentCharacter = CharacterPrefabs[0];
+        ActivateCharacter(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check input then switch character to the next one
+        if (Input.GetButtonUp("Fire2"))
+        {
+            SwitchCharacters();
+        }
+    }
+
+    void ActivateCharacter(int characterIndex)
+    {
+        Debug.Log("Activate character at index " + characterIndex);
+        // Save the posistion of the current character
+        charPosition = currentCharacter.transform.position;
+
+        // disable all character except for the selected one
+        foreach (GameObject character in CharacterPrefabs) { character.SetActive(false); }
+        currentCharacter = CharacterPrefabs[characterIndex];
+        // move the character and update camera before activating
+        currentCharacter.transform.position = charPosition;
+        MainCamera.target = currentCharacter.transform;
+
+        CharacterPrefabs[characterIndex].SetActive(true);
+    }
+
+    // Cycle through and switch to different characters
+    void SwitchCharacters()
+    {
+        // Get the index of the currently active character
+        int index = Array.IndexOf(CharacterPrefabs, currentCharacter);
+        // update index by 1, wrap back to 0 if needed
+        index += 1;
+        if (index >= CharacterPrefabs.Length) { index = 0; }
+        // activate the character
+        ActivateCharacter(index);
+    }
+}
