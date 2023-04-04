bee9e5fdb535da672b551ba457d340ead73867e6
qioqvi <vntommyvn@gmail.com>
2023-04-04 23:41:24 +1000

    Basic Moving Platform

---
+++ Assets/Scripts/PlaneControl.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneControl : MonoBehaviour
+{
+    public float Speed;
+
+    public float destoryPosition = -1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + Vector3.back * Speed * Time.deltaTime;
+        if (transform.position.z < destoryPosition)
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/ScrollingPlane.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingPlane : MonoBehaviour
+{
+
+    public GameObject Plane;
+
+    public float StartingZ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScrollPlane();
+    }
+
+    void ScrollPlane()
+    {
+        GameObject.Instantiate(Plane, new Vector3(0, 0, StartingZ), Quaternion.identity);
+        Invoke("ScrollPlane", 10);
+    }
+}
