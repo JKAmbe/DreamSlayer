871c643a54dc45f32aa8d6d4d4ab4cb425365da4
yuzusiooooo <70190614+yuzusiooooo@users.noreply.github.com>
2023-04-25 17:52:51 +1000

    parry now uses a window of opportunity system
    using parry will start a window of opportunity
    during this window of opportunity the player can parry incoming attacks
    if a parry was a success, there's a cooldown

--- Assets/Scripts/CharacterSpecialAbility/AbilityParry.cs
+++ Assets/Scripts/CharacterSpecialAbility/AbilityParry.cs
@@ -8,7 +8,10 @@ public class AbilityParry : SpecialAbility
     public GameObject ParryCheckHitbox;
     [Header("Parry ability Stats")]
     public float parryCooldown = 0.0f;
+    public float parryWindow = 0.0f;
     float cParryCooldown = -1.0f;
+    float cParryWindow = -1.0f;
+    bool bParrySuccess = false;
     [Header("Parry Bullet Stats")]
     public float parryDamage = 0.0f;
     public float parrySpeed = 0.0f;
@@ -26,45 +29,69 @@ public class AbilityParry : SpecialAbility
 
     void UpdateParry()
     {
-        // parry cooldown
-        if (cParryCooldown >= 0.0f)
-        {
-            bAllowAbilityOn = false;
-            cParryCooldown -= Time.deltaTime;
-            if (cParryCooldown <= 0.0f)
-            {
-                cParryCooldown = -1.0f;
-            }
-        }
+        // ability can be used depending on if there's a cooldown
+        bAllowAbilityOn = cParryCooldown <= 0.0f;
+
         if (bAllowAbilityOn)
         {
-            ParryCheckHitbox.SetActive(bAbilityOn);
-        } else { ParryCheckHitbox.SetActive(false); }
+            if (cParryWindow != 0.0f)
+            {
+                cParryWindow -= Time.deltaTime;
+                // turn parry window off 
+                if (cParryWindow <= 0.0f)
+                {
+                    bAbilityOn = false;
+                    cParryWindow = 0.0f;
+                    // if the parry was success at this point force a cooldown
+                    if (bParrySuccess)
+                    {
+                        cParryCooldown = parryCooldown;
+                    }
+                }
+            }
+        } else {
+            bAbilityOn = false; 
+            if (cParryCooldown != 0.0f)
+            {
+                cParryCooldown -= Time.deltaTime;
+                if (cParryCooldown <= 0.0f)
+                {
+                    bParrySuccess = false;
+                    bAllowAbilityOn = true;
+                    cParryCooldown = 0.0f;
+                }
+            }
+
+        }
+
+        // allow parry to be used 
+        ParryCheckHitbox.SetActive(bAbilityOn);
     }
 
     override public void useSpecialAbility()
     {
-        bAbilityOn = true;
+        // if the ability isnt already on, turn it on and start parry attempt
+        if (!bAbilityOn)
+        {
+            bAbilityOn = true;
+            cParryWindow = parryWindow;
+        }
+
     }
 
     public override void unuseSpecialAbility()
     {
-        bAbilityOn = false;
+
     }
     public void ParryBullet(EnemyBullet EnemyBullet)
     {
-        // allow parry only when its not on a cooldown
-        if (bAllowAbilityOn)
-        {
-            // force cooldown
-            cParryCooldown = parryCooldown;
-            // Parry the enemy bullet so it damages themselves
-            EnemyBullet.bParried = true;
-            EnemyBullet.GetComponent<Rigidbody>().velocity *= -1 * parrySpeed;
-            EnemyBullet.gameObject.layer = 9;
-            EnemyBullet.includeTag = "Enemy";
-            EnemyBullet.damage = parryDamage;
-        }
+        bParrySuccess = true;
+        // Parry the enemy bullet so it damages themselves
+        EnemyBullet.bParried = true;
+        EnemyBullet.GetComponent<Rigidbody>().velocity *= -1 * parrySpeed;
+        EnemyBullet.gameObject.layer = 9;
+        EnemyBullet.includeTag = "Enemy";
+        EnemyBullet.damage = parryDamage;
 
     }
 }
