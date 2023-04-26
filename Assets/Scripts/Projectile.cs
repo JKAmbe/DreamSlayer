c235835eaacef927c78398880c6bcacb11df6b72
qioqvi <vntommyvn@gmail.com>
2023-04-26 16:07:20 +1000

    Prototype Destroyable Obstacles

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -18,6 +18,7 @@ public class Projectile : MonoBehaviour
         Debug.Log(other.name);
         if (other.CompareTag(includeTag) && other.GetComponentInChildren<HealthBar>())
             other.GetComponentInChildren<HealthBar>().TakeDamage(damage);
+        
     }
 
 }
