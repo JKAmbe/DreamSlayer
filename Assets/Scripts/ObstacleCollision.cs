d012f8da70167b8194619aed2a020c9d3cfb40e2
qioqvi <vntommyvn@gmail.com>
2023-04-27 00:18:39 +1000

    2nd Prototype Obstacles

--- Assets/Scripts/ObstacleCollision.cs
+++ Assets/Scripts/ObstacleCollision.cs
@@ -6,10 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class ObstacleCollision : MonoBehaviour
 {
-    public float alpha1 = 0.8f;
-    public float alpha2 = 0.6f;
-    public float alpha3 = 0.4f;
-    public float alpha4 = 0.2f;
+    public float alpha = 0.2f;
     public int count = 0;
     
     private Material objectMaterial;
@@ -29,47 +26,17 @@ public class ObstacleCollision : MonoBehaviour
 
         if (other.gameObject.layer == LayerMask.NameToLayer("Player Projectile"))
         {
-            if (count == 0)
+            while (count != 5)
             {
                 Destroy(other.gameObject);
                 Color color = objectMaterial.color;
-                color.a = alpha1;
-                objectMaterial.color = color;
+                color.a -= alpha;
                 count++;
                 Debug.Log("Count: " + count);
             }
-            if (count == 1)
-            {
-                Destroy(other.gameObject);
-                Color color = objectMaterial.color;
-                color.a = alpha2;
-                objectMaterial.color = color;
-                count++;
-                Debug.Log("Count: " + count);
-            }
-            if (count == 2)
-            {
-                Destroy(other.gameObject);
-                Color color = objectMaterial.color;
-                color.a = alpha3;
-                objectMaterial.color = color;
-                count++;
-                Debug.Log("Count: " + count);
-            }
-            if (count == 3)
-            {
-                Destroy(other.gameObject);
-                Color color = objectMaterial.color;
-                color.a = alpha4;
-                objectMaterial.color = color;
-                count++;
-                Debug.Log("Count: " + count);
-            }
-            if (count == 4)
-            {
-                Destroy(other.gameObject);
-                Destroy(gameObject);
-            }
+
+            count = 0;
+            Destroy(gameObject);
         }
     }
 }
