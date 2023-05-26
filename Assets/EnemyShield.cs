af9b9b16675b94bba3ee749b46e8547bd11e6f4d
JKAmbe <89562604+JKAmbe@users.noreply.github.com>
2023-05-26 18:35:25 +1000

    Shield Anim

--- Assets/EnemyShield.cs
+++ Assets/EnemyShield.cs
@@ -6,25 +6,12 @@ public class EnemyShield : MonoBehaviour
 {
     public GameObject shieldArea;
     Vector3 OriginalScale;
-    Vector3 CurrentScale;
     public float reductionSpeed = 0.5f;
     private void Start()
     {
-        shieldArea = transform.GetChild(0).gameObject;
         OriginalScale = shieldArea.transform.localScale;
-        CurrentScale = OriginalScale;
     }
 
-    private void FixedUpdate()
-    {
-        CurrentScale.x = Mathf.Clamp(CurrentScale.x - Time.deltaTime*reductionSpeed, 0, OriginalScale.x);
-        CurrentScale.z = Mathf.Clamp(CurrentScale.z - Time.deltaTime * reductionSpeed, 0, OriginalScale.x);
-        shieldArea.transform.localScale = CurrentScale;
-        if (CurrentScale.x <= 0)
-            shieldArea.GetComponent<Collider>().enabled = false;
-        else
-            shieldArea.GetComponent<Collider>().enabled = true;
-    }
     private void OnTriggerEnter(Collider other)
     {     
         if (other.gameObject.layer == LayerMask.NameToLayer("Player Projectile"))
@@ -32,7 +19,7 @@ public class EnemyShield : MonoBehaviour
             Vector3 projectilePosition = other.transform.position;
             projectilePosition.z = transform.parent.position.z;
             GetComponentInParent<ShieldEnemy>().nextPosition = projectilePosition;
-            CurrentScale = OriginalScale;
+            shieldArea.transform.localScale = OriginalScale;
         }
     }
 }
--- Assets/Sh.cs
+++ Assets/Sh.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 
 public class Sh : MonoBehaviour
 {
+    public float reductionSpeed = 0.5f;
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Player Projectile"))
         {
             other.GetComponent<Collider>().enabled = false;
-            //other.GetComponent<Renderer>().enabled = false;
-            Destroy(other.gameObject, 0.5f);
+            other.GetComponentInChildren<Renderer>().enabled = false;
+            other.GetComponentInChildren<TrailRenderer>().enabled = false;
+            other.GetComponentInChildren<ParticleSystemRenderer>().enabled = false;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (transform.localScale.x >= 0)
+        { 
+            transform.localScale -= Time.deltaTime * Vector3.one * reductionSpeed;
+            GetComponent<Collider>().enabled = true;
+        }
+        else
+        {
+            GetComponent<Collider>().enabled = false;
         }
     }
 }
