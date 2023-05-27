b71666b073f65e96e6a37981595389d4692a27a7
LinnKyaw <lynn.kyaw.2003@gmail.com>
2023-05-27 15:15:31 +1000

    more changes maded

--- Assets/Scripts/HealthPickUp.cs
+++ Assets/Scripts/HealthPickUp.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class HealthPickUp : MonoBehaviour
 {
     public float HealAmount = 50.0f;
+    public AudioSource audioSource;
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
+        audioSource.Play();
         if (other.GetComponentInChildren<HealthBar>())
         {
             other.GetComponentInChildren<HealthBar>().Heal(HealAmount);
